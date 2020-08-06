2ee3797933d7f6cb35310a465840cf3521e308c4
Gustavo Pichorim Boiko <gustavo.boiko@gmail.com>
2020-08-05 21:57:24 -0300

    Implement the sklearn estimator interface
    This will be used by the grid search algorithm to tweak the parameters

--- denoisers/denoiser.py
+++ denoisers/denoiser.py
@@ -1,14 +1,25 @@
 # Denoiser: base class for image denoisers
 
 from abc import ABC, abstractmethod
+import numpy as np
+from metrics import default_metric
 
 class Denoiser(ABC):
-    """ Base class for all denoiser objects
+    """
+    Base class for all denoiser objects
 
-        Sub-classes need to implement the @ref denoise() """
+    Sub-classes need to implement the @ref denoise()
+    """
 
     name = "Denoiser"
     description = "Base class for denoisers"
+
+    param_grid = {}
+
+    def __init__(self, **kwargs):
+        # if used from sklearn (via score) use a default metric
+        self._metric = None
+        self.set_params(**kwargs)
 
     @abstractmethod
     def denoise(self, image):
@@ -25,3 +36,27 @@ class Denoiser(ABC):
         :return: swapped image
         """
         return image[:, :, ::-1]
+
+    # implement Scikit-learn estimator interface to make it easier to do grid search on denoisers
+    def get_params(self, deep=False):
+        return {p: getattr(self, p) for p in self.param_grid.keys()}
+
+    def set_params(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        return self
+
+    def fit(self, noisy_images, ref_images):
+        return self
+
+    def predict(self, noisy_images):
+        return np.array([self.denoise(noisy) for noisy in noisy_images])
+
+    def score(self, noisy_images, ref_images):
+        results = self.predict(noisy_images)
+
+        if not self._metric:
+            self._metric = default_metric()
+
+        # return the average value of the default metric for the denoised images
+        return np.array([self._metric.compare(ref, res) for ref, res in zip(ref_images, results)]).mean()
